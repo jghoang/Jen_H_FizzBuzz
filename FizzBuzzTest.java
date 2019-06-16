0a6839ac80e43b0b01285e6c70c79d1c15f5e9a5
jghoang <50341293+jghoang@users.noreply.github.com>
2019-06-16 14:31:57 -0500

    Add files via upload

---
+++ FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+    public static void main (String[] arg){
+        FizzBuzz iD = new FizzBuzz();
+        String fizzBuzz = iD.fizzBuzz(30);
+        System.out.println(fizzBuzz);
+
+    }
+}
